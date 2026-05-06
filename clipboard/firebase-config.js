53cc9a6ef33b9a68de73d7f8d8a2a2cdde167578
Yankai Zhao <yaz624@lehigh.edu>
2026-05-06 16:27:10 -0400

    Configure Firebase web app

--- clipboard/firebase-config.js
+++ clipboard/firebase-config.js
@@ -1,6 +1,9 @@
 window.CLIPBOARDSTACK_FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  appId: "",
+  apiKey: "AIzaSyD6fI6U0l4CJv7PdOfTvW3lnHZwmPj_L-Y",
+  authDomain: "clipboardstack.firebaseapp.com",
+  projectId: "clipboardstack",
+  storageBucket: "clipboardstack.firebasestorage.app",
+  messagingSenderId: "908435468416",
+  appId: "1:908435468416:web:7846bd0a0ba796430ef665",
+  measurementId: "G-TE5ZJNHLNL",
 };
